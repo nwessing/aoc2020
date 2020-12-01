33a0f3dfe48fbef4ca1f7c97e72d1abaa6fa8279
Nick Wessing <nick@headway.io>
2020-12-01 07:58:01 -0600

    Day 1 Exercise 2

--- day1/main.c
+++ day1/main.c
@@ -2,6 +2,7 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "stdint.h"
+#include "string.h"
 
 int32_t *read_file(const char *filename, uint32_t *count) {
   FILE *file = fopen(filename, "rb");
@@ -34,24 +35,12 @@ int32_t *read_file(const char *filename, uint32_t *count) {
 }
 
 int32_t compare(const void *a, const void *b) {
-    int32_t a_value =  *(int32_t*)a;
-    int32_t b_value =  *(int32_t*)b;
-   return a_value - b_value;
-
+  int32_t a_value =  *(int32_t*)a;
+  int32_t b_value =  *(int32_t*)b;
+  return a_value - b_value;
 }
 
-int main(int argc, char **argv) {
-  if (argc < 2) {
-    printf("Please supply input file\n");
-    return 1;
-  }
-
-  char *input_path = argv[1];
-  uint32_t count;
-  int32_t *inputs = read_file(input_path, &count);
-
-  qsort(inputs, count, sizeof(inputs[0]), compare);
-
+void exercise_one(int32_t *inputs, uint32_t count) {
   uint32_t i_min = 0;
   uint32_t i_max = count - 1;
   while (i_min != i_max) {
@@ -60,7 +49,7 @@ int main(int argc, char **argv) {
       printf("SOLUTION FOUND!\n");
       printf("%i + %i == 2020\n", inputs[i_min], inputs[i_max]);
       printf("%i * %i == %i\n", inputs[i_min], inputs[i_max], inputs[i_min] * inputs[i_max]);
-      return 0;
+      return;
     }
     else if (sum > 2020) {
       --i_max;
@@ -69,6 +58,55 @@ int main(int argc, char **argv) {
       ++i_min;
     }
   }
+
+  printf("NO SOLUTION FOUND!\n");
+}
+
+void exercise_two(int32_t *inputs, uint32_t count) {
+  // Brute force
+  for (uint32_t i = 0; i < count; ++i) {
+    for (uint32_t j = 0; j < count; ++j) {
+      for (uint32_t k = 0; k < count; ++k) {
+        if (i == j || i == k || j == k) {
+          continue;
+        }
+
+        int32_t x = inputs[i];
+        int32_t y = inputs[j];
+        int32_t z = inputs[k];
+        if (x + y + z == 2020) {
+          printf("SOLUTION FOUND!\n");
+          printf("%i + %i + %i == 2020\n", x, y, z);
+          printf("%i * %i * %i == %i\n", x, y, z, x * y * z);
+          return;
+        }
+      }
+    }
+  }
+
+  printf("NO SOLUTION FOUND!\n");
+}
+
+int main(int argc, char **argv) {
+  if (argc < 3) {
+    printf("Usage: aocd1 (1|2) input_file_name \n");
+    printf("where (1|2) selects exercise 1 or 2\n");
+    printf("where input_file_name is a file path containing a line separated list of valid 32-bit integers\n");
+    return 1;
+  }
+
+  char *input_path = argv[2];
+  uint32_t count;
+  int32_t *inputs = read_file(input_path, &count);
+
+  qsort(inputs, count, sizeof(inputs[0]), compare);
+
+  if (strcmp(argv[1], "1") == 0) {
+    exercise_one(inputs, count);
+  } else if (strcmp(argv[1], "2") == 0) {
+    exercise_two(inputs, count);
+  }
+
 
   return 0;
 }
