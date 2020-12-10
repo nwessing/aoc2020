dcaaed0aad5ef8df36eb2aff4d8922f2cd9731ac
Nick Wessing <nick@headway.io>
2020-12-10 07:36:03 -0600

    run all

---
+++ run_all.sh
@@ -0,0 +1,8 @@
+./bin/day1/day1 day1/input.txt
+./bin/day2/day2 day2/input.txt
+./bin/day3/day3 day3/input.txt
+./bin/day4/day4 day4/input.txt
+./bin/day5/day5 day5/input.txt
+./bin/day6/day6 day6/input.txt
+./bin/day7/day7 day7/input.txt
+./bin/day8/day8 day8/input.txt
